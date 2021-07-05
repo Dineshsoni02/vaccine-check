e55cbdd37a63d92614378570a6b7de6f9f393e6d
unknown <dineshsoni8569@gmail.com>
2021-07-05 18:15:16 +0530

    j5

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
-import { Grid, Button, Backdrop, Box, Paper } from "@material-ui/core";
+import {
+  Grid,
+  Button,
+  Backdrop,
+  Box,
+  Container,
+} from "@material-ui/core";
 import Centers from "./components/Centers";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "./App.css";
+
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -55,20 +62,145 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        style={{ width: "30%" }}
-        
-      >
-        <Paper elevation={3} style={{ borderRadius: "25px" }}>
-          <Grid item xs={12} style={{ background: "blue" }}>
-            <h1 style={{ color: "white" }}>VACCINATION APP</h1>
-          </Grid>
+    // <div className="App">
+    //   <Grid
+    //     container
+    //     direction="column"
+    //     justify="center"
+    //     style={{ width: "30%" }}
 
-          <Grid item xs={12}>
+    //   >
+    //     <Paper elevation={3} style={{ borderRadius: "25px" }}>
+    //       <Grid item xs={12} style={{ background: "blue" }}>
+    //         <h1 style={{ color: "white" }}>VACCINATION APP</h1>
+    //       </Grid>
+
+    //       <Grid item xs={12}>
+    //         <Box
+    //           display="flex"
+    //           flexDirection="row"
+    //           alignItems="center"
+    //           justifyContent="center"
+    //           // marginLeft="-2rem"
+    //           // style={{ background: "aquamarine" }}
+    //         >
+    //           {/* Demo */}
+
+    //           <h3>Select date : </h3>
+    //           <Button
+    //             variant="outlined"
+    //             color="primary"
+    //             onClick={handleToggle}
+    //             style={{ border: "none",color:"#318AF4",fontSize:"1.1rem",marginTop:"8px"}}
+    //           >
+    //             {`${dateValue.getDate()}-${
+    //               dateValue.getMonth() + 1
+    //             }-${dateValue.getFullYear()}`}
+    //           </Button>
+    //           <Backdrop
+    //             className={classes.backdrop}
+    //             open={open}
+    //             onClick={handleClose}
+    //           >
+    //             <Calendar
+    //               value={dateValue}
+    //               defaultView="month"
+    //               minDate={new Date()}
+    //               onClickDay={(value) => {
+    //                 setDateValue(value);
+    //               }}
+    //             />
+    //           </Backdrop>
+
+    //           {/* sasdasd */}
+    //         </Box>
+    //       </Grid>
+
+    //       <Grid
+    //         item
+    //         xs={12}
+    //         // style={{ background: "grey" }}
+    //       >
+    //         <Box
+    //           display="flex"
+    //           flexDirection="row"
+    //           alignItems="center"
+    //           justifyContent="center"
+    //         >
+    //           <h3>Enter Pin:</h3>
+
+    //           <input
+    //             style={{
+    //               border: "none",
+    //               marginLeft: "0.5rem",
+    //               marginTop:"5px",
+    //               outlineColor: "#318AF4",
+    //               lineHeight: "1rem",
+    //               fontSize:"1.08rem",
+    //               width:"5.5rem",
+    //               padding:"5px",
+    //               color:"#093EF0"
+    //             }}
+    //             type="tel"
+    //             placeholder="Enter PIN"
+    //             value={pinValue}
+    //             maxLength="6"
+    //             onChange={(e) => {
+    //               const value = e.target.value;
+    //               const char = value.slice(-1);
+    //               if ((char >= "0" && char <= "9") || value === "") {
+    //                 setPinValue(value);
+    //               }
+    //             }}
+    //             onKeyUp={(e) => (e.keyCode === 13 ? searchHandler() : "")}
+    //           ></input>
+    //         </Box>
+    //       </Grid>
+    //       <Grid item xs={12} md={12} lg={12} style={{ textAlign: "center" }}>
+    //         <small className="field-error-msg">{errorMsg}</small>
+    //       </Grid>
+    //       <Grid item xs={12}>
+    //         <Button variant="outlined" color="primary" onClick={searchHandler} style={{borderRadius:"25px", lineHeight:"20px", marginBottom:"12px", borderColor:"#318AF4" }}>
+    //           Search
+    //         </Button>
+    //       </Grid>
+    //     </Paper>
+    //   </Grid>
+    //   <Grid
+    //     container
+    //     style={{
+    //       width: "50%",
+    //     }}
+
+    //   >
+    //     {centers ? (
+    //       centers.length === 0 ? (
+    //         <h2>No Centers found at this PIN code and Date.</h2>
+    //       ) : (
+    //         centers.map((item, i) => (
+    //           <Centers key={i} data={item} sessions={item.sessions} />
+    //         ))
+    //       )
+    //     ) : (
+    //       ""
+    //     )}
+    //   </Grid>
+    // </div>
+
+    <div className="App">
+      <Container
+        style={{
+          height: "90vh",
+          width: "90% ",
+          padding: "0",
+        }}
+        className="hero"
+      >
+        <div
+          style={{ width: "50%", height: "100%", position: "relative" }}
+          className="left_portion"
+        >
+          <div className="outer">
             <Box
               display="flex"
               flexDirection="row"
@@ -84,7 +216,12 @@ function App() {
                 variant="outlined"
                 color="primary"
                 onClick={handleToggle}
-                style={{ border: "none",color:"#318AF4",fontSize:"1.1rem",marginTop:"8px"}}
+                style={{
+                  border: "none",
+                  color: "#318AF4",
+                  fontSize: "1.1rem",
+                  marginTop: "8px",
+                }}
               >
                 {`${dateValue.getDate()}-${
                   dateValue.getMonth() + 1
@@ -104,16 +241,8 @@ function App() {
                   }}
                 />
               </Backdrop>
-
-              {/* sasdasd */}
             </Box>
-          </Grid>
-
-          <Grid
-            item
-            xs={12}
-            // style={{ background: "grey" }}
-          >
+            {/* ////////////////////////////////////////////////////////////// */}
             <Box
               display="flex"
               flexDirection="row"
@@ -126,13 +255,13 @@ function App() {
                 style={{
                   border: "none",
                   marginLeft: "0.5rem",
-                  marginTop:"5px",
+                  marginTop: "5px",
                   outlineColor: "#318AF4",
                   lineHeight: "1rem",
-                  fontSize:"1.08rem",
-                  width:"5.5rem",
-                  padding:"5px",
-                  color:"#093EF0"
+                  fontSize: "1.08rem",
+                  width: "5.5rem",
+                  padding: "5px",
+                  color: "#093EF0",
                 }}
                 type="tel"
                 placeholder="Enter PIN"
@@ -148,37 +277,46 @@ function App() {
                 onKeyUp={(e) => (e.keyCode === 13 ? searchHandler() : "")}
               ></input>
             </Box>
+            {/* ////////////////////////////////////////////////////////////////////////// */}
+            <div style={{ textAlign: "center" }}>
+              <h4 className="field-error-msg">{errorMsg}</h4>
+            </div>
+            <div>
+              <Button
+                variant="outlined"
+                color="primary"
+                onClick={searchHandler}
+                style={{
+                  borderRadius: "25px",
+                  lineHeight: "20px",
+                  marginBottom: "12px",
+                  borderColor: "#318AF4",
+                }}
+              >
+                Search
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Container>
+          <Grid
+            container
+            style={{
+              width: "50%",
+            }}
+          >
+            {centers ? (
+              centers.length === 0 ? (
+                <h2>No Centers found at this PIN code and Date.</h2>
+              ) : (
+                centers.map((item, i) => (
+                  <Centers key={i} data={item} sessions={item.sessions} />
+                ))
+              )
+            ) : (
+              ""
+            )}
           </Grid>
-          <Grid item xs={12} md={12} lg={12} style={{ textAlign: "center" }}>
-            <small className="field-error-msg">{errorMsg}</small>
-          </Grid>
-          <Grid item xs={12}>
-            <Button variant="outlined" color="primary" onClick={searchHandler} style={{borderRadius:"25px", lineHeight:"20px", marginBottom:"12px", borderColor:"#318AF4" }}>
-              Search
-            </Button>
-          </Grid>
-        </Paper>
-      </Grid>
-      <Grid
-        container
-        style={{
-          width: "50%",
-        }}
-       
-        
-      >
-        {centers ? (
-          centers.length === 0 ? (
-            <h2>No Centers found at this PIN code and Date.</h2>
-          ) : (
-            centers.map((item, i) => (
-              <Centers key={i} data={item} sessions={item.sessions} />
-            ))
-          )
-        ) : (
-          ""
-        )}
-      </Grid>
     </div>
   );
 }
